db26705ab406d5590a7c90f42a31f87aac005ef1
Connor Lagana <connor.lagana@gmail.com>
2020-06-04 15:20:43 -0400

    fourohfive

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,8 +5,21 @@ import logo1 from "../../logos/logo1.png";
 class Register extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      emailConfirm: "",
+      username: "",
+      password: "",
+    };
   }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
     return (
       <div className="Register">
@@ -14,7 +27,50 @@ class Register extends Component {
           <img id="circleLogo" src={circleLogo} />
           <img id="wordLogo" src={logo1} />
         </div>
-        <form></form>
+        <form>
+          <input
+            id="myName"
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            placeholder="Enter your email"
+            // onfocus="this.select()"
+            required
+          />
+          <input
+            id="myName"
+            type="text"
+            name="username"
+            value={this.state.emailConfirm}
+            onChange={this.handleChange}
+            placeholder="Confirm your email"
+            // onfocus="this.select()"
+            required
+          />
+          <input
+            id="myName"
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            placeholder="Enter a username"
+            // onfocus="this.select()"
+            required
+          />
+          <input
+            id="myName"
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            placeholder="Create a password"
+            required
+          />
+          <button className="buttonfx curtainup" onClick={this.registerUser}>
+            Sign Up
+          </button>
+        </form>
       </div>
     );
   }
