35591fe4514cc30acea332de38c49d12f85edd5e
Connor Lagana <connor.lagana@gmail.com>
2020-09-17 21:33:50 -0400

    litty

--- src/components/Home/HomeMixes.js
+++ src/components/Home/HomeMixes.js
@@ -1,92 +1,36 @@
 import React from "react";
-// import share from "../../icons/share.png";
-// import play from "../../icons/play.png";
-// import pause from "../../icons/pause.png";
+import share from "../../icons/share.png";
+import play from "../../icons/play.png";
+import pause from "../../icons/pause.png";
 import plus from "../../icons/plus.png";
-// import ReactAudioPlayer from "react-audio-player";
-// import mix1 from "../../mixes/Mix1.mp3";
-// import mix2 from "../../mixes/Mix2.mp3";
-// import mix3 from "../../mixes/Mix3.mp3";
-// import mix4 from "../../mixes/Mix4.mp3";
+import ReactAudioPlayer from "react-audio-player";
+import mix1 from "../../mixes/Mix1.mp3";
+import mix2 from "../../mixes/Mix2.mp3";
+import mix3 from "../../mixes/Mix3.mp3";
+import mix4 from "../../mixes/Mix4.mp3";
 
 import { Link } from "react-router-dom";
-// const mixes = [mix1, mix2, mix3, mix4];
+const mixes = [mix1, mix2, mix3, mix4];
 
 // const songUrl = "https://media.vocaroo.com/mp3/1ohtk7IZgs5";
-
-const mixes = [
-  {
-    id: 1,
-    title: "",
-    mix: "mix1",
-    createdAt: "2020-09-07T16:36:16.791Z",
-    updatedAt: "2020-09-07T16:36:16.791Z",
-    userId: "1",
-  },
-  {
-    id: 2,
-    title: "",
-    mix: "mix1",
-    createdAt: "2020-09-07T18:46:01.250Z",
-    updatedAt: "2020-09-07T18:46:01.250Z",
-    userId: "2",
-  },
-  {
-    id: 3,
-    title: "dick bangers vol 3",
-    mix: "mix1",
-    createdAt: "2020-09-07T18:47:14.412Z",
-    updatedAt: "2020-09-07T18:47:14.412Z",
-    userId: "2",
-  },
-  {
-    id: 4,
-    title: "Fuckme",
-    mix: "mix1",
-    createdAt: "2020-09-07T18:50:56.098Z",
-    updatedAt: "2020-09-07T18:50:56.098Z",
-    userId: "3",
-  },
-  {
-    id: 5,
-    title: "Swag Daddy's Pump Jam",
-    mix: "mix1",
-    createdAt: "2020-09-07T19:23:10.284Z",
-    updatedAt: "2020-09-07T19:23:10.284Z",
-    userId: "10",
-  },
-  {
-    id: 6,
-    title: "",
-    mix: "mix1",
-    createdAt: "2020-09-09T19:22:15.368Z",
-    updatedAt: "2020-09-09T19:22:15.368Z",
-    userId: "11",
-  },
-  {
-    id: 7,
-    title: "",
-    mix: "mix1",
-    createdAt: "2020-09-09T19:22:16.029Z",
-    updatedAt: "2020-09-09T19:22:16.029Z",
-    userId: "11",
-  },
-  {
-    id: 8,
-    title: "Con lagans mix",
-    mix: "mix1",
-    createdAt: "2020-09-15T03:35:51.477Z",
-    updatedAt: "2020-09-15T03:35:51.477Z",
-    userId: "16",
-  },
-];
 
 const HomeMixes = (props) => {
   return (
     <div className="HomeMixes">
-      {mixes.map((song, songIdx) => (
+      {props.songs.map((song, songIdx) => (
         <div className="song" key={songIdx}>
-          hello
+          <div className="leftSong">
+            <div id="square" className="grad4" />
+            <div id="songName">{song.title}</div>
+            <ReactAudioPlayer src={mixes[1]} controls />
+          </div>
+          <div className="rightSong">
+            <img src={share} id="share" alt="" />
+            <div>
+              {song.min}:{song.sec}
+            </div>
+            <img src={play} id="play" alt="" />
+          </div>
         </div>
       ))}
 
