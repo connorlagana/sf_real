0a0e4ec8e131c85b49259bbb552555e7f3021e73
Connor Lagana <connor.lagana@gmail.com>
2020-11-02 21:17:56 -0500

    made create mix button bigger

--- src/components/NewMix/CreateMixButton.js
+++ src/components/NewMix/CreateMixButton.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 const CreateMixButton = (props) => {
   return (
     <div className="CreateMixButton">
-      <button className={props.butHide1}
+      <button id="bttn" className={props.butHide1}
         onClick={(e) => {
           props.handleCreateMix(e);
         }}
